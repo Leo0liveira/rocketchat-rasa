43f67a97883d23354bd310247a09c4bc012a5fc0
jpwakugawa <jp_wakugawa@outlook.com>
2022-05-18 15:07:26 -0400

    chore: pegando header

---
+++ scripts/create_bot.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import json
+import logging
+import requests
+
+admin_name = "boss"
+admin_password = "boss"
+
+bot_name = "rasa_bot"
+bot_password = "rasa_bot"
+bot_email = bot_name + '@email.com'
+
+host = "http://localhost:3000/"
+user_header = None
+
+path = "/api/v1/login"
+
+def get_authentication_token():
+    login_data = {"username": admin_name, "password": admin_password}
+    response = requests.post(host + path, data=login_data)
+
+    if response.json()["status"] == "success":
+        logging.info("Login suceeded")
+
+        authToken = response.json()["data"]["authToken"]
+        userId = response.json()["data"]["userId"]
+        user_header = {
+            "X-Auth-Token": authToken,
+            "X-User-Id": userId,
+            "Content-Type": "application/json"
+        }
+
+        return user_header
+    else:
+        print(response)
+
+def create_bot_user():
+    user_info = {
+        "name": bot_name,
+        "email": bot_email,
+        "password": bot_password,
+        "username": bot_name,
+        "requirePasswordChange": False,
+        "sendWelcomeEmail": True, "roles": ["bot"]
+    }
+
+    create_user_response = requests.post(
+        host + '/api/v1/users.create',
+        data=user_info,
+        headers=user_header
+    )
+
+    if create_user_response.json()['success'] is True:
+        logging.info('User has been sucessfully created!')
+    else:
+        logging.error('Error while creating bot user!')
+
+user_header = get_authentication_token()
+print(user_header)
